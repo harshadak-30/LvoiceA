ed2f3093cf4bd878fe47152a872af6fda969e7c0
Harshada Kahandal <harshukahandal30@gmail.com>
2025-07-16 23:45:48 +0530

    Update server.js

--- voicea/backend/server.js
+++ voicea/backend/server.js
@@ -13,8 +13,7 @@ app.use(cors({ origin: "*", methods: ["GET", "POST", "DELETE"], allowedHeaders: 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 app.use(morgan("dev")); 
-mongoose
-  .connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
